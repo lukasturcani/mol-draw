c4f52f96de4fd22fb00da447401a2deb3a517230
Lukas Turcani <lukasturcani93@gmail.com>
2020-06-19 13:09:45 +0200

    removed plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 
@@ -8,12 +7,6 @@ module.exports = {
         path: path.join(__dirname, 'dist'),
         filename: 'molDraw.js',
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'molDraw',
-            template: './src/template.html',
-        }),
-    ],
     mode: 'development',
     resolve: {
         extensions: ['.js', '.json'],
